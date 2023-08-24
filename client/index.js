85d24cbf6b62b2a3be088c4de0a251f1bd5eee84
Manuel <maxpedraza17@gmail.com>
2023-08-24 16:08:04 -0400

    Start of the console interface

--- client/index.js
+++ client/index.js
@@ -1,7 +1,94 @@
-const {io} = require("socket.io-client");
+const { io, connect } = require("socket.io-client");
+let socket = undefined;
+const readline = require("node:readline/promises").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
 
-const socket = io("http://localhost:3000");
+let userName = "";
+let connectedToServer = false;
 
-socket.on("connection", () => {
-    console.log(socket.id);
-})
+async function askForName() {
+
+    do {
+        console.clear();
+        const answer = await readline.question("Enter a player name: ");
+        userName = answer.trim();
+    } while (userName === "" || userName === undefined)
+}
+
+readline.on("close", () => {
+    console.log("Read line closed");
+});
+
+async function socketInit() {
+    console.log("Connecting to server");
+    socket = io("http://localhost:3000");
+    
+    socket.on("connect", () => {
+        console.log("Connected to server");
+
+        menu();
+    });
+
+}
+
+function showMenu() {
+    console.log(
+        `Options:
+    1. See Room List
+    2. Create Room`
+    );
+}
+
+function seeRoomListMenu(){
+
+}
+
+async function menu(){
+    let userAnswer = "";
+
+    while (userAnswer !== "exit") {
+        showMenu();
+        userAnswer = (await readline.question("Choice (type 'exit' to quit the program): ")).trim();
+        console.clear();
+
+        switch (userAnswer) {
+            case "1":
+                seeRoomListMenu();
+                break;
+            case "2":
+                break;
+            case "exit":
+                continue;
+            default:
+                console.log("Not a valid option");
+                break;
+        }
+
+    }
+}
+
+function creatRoom() {
+
+}
+
+function getRoomList(){
+    return ['1', '2'];
+}
+
+function showRoomList(){
+
+}
+
+function joinRoom(roomName) {
+
+}
+
+async function main() {
+    await askForName();
+    socketInit();
+}
+
+main();
+
--- server/index.js
+++ server/index.js
@@ -1,18 +1,23 @@
-const { Server } = require("socket.io")
+const { Server } = require("socket.io");
 
 const io = new Server();
+
 // const io = new Server(3000, {options});
 
 io.on("connection", (socket) => {
 
-    console.log("Socket connected", socket.id);
+    console.log("Socket connected " + socket.id);
 
     socket.on("disconnect", () => {
         console.log("Socket disconnected");
-    })
+    });
 
 });
+
+
+
 
 io.listen(3000, () => {
     console.log("listenning on port *: 3000");
 });
+
