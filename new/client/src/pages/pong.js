86a7b7e43df860be04b773c11428add55c4a0dea
Manuel <maxpedraza17@gmail.com>
2023-09-27 23:10:04 -0400

    Start of the pong page logic

--- new/client/src/pages/pong.js
+++ new/client/src/pages/pong.js
@@ -1,15 +1,23 @@
+import { Plaster } from 'next/font/google';
 import React, { useEffect, useRef } from 'react'
 
 export default function Pong() {
 
     const canvasRef = useRef(null);
 
-    const draw = (ctx, frameCount) => {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-        ctx.fillStyle = '#000000'
-        ctx.beginPath()
-        ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
+    const drawBg = (ctx, canvas) => {
+        ctx.fillStyle = '#000000';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
         ctx.fill()
+
+    }
+
+    const drawPlayer = (ctx, player) => {
+        console.log(player.x - (player.width / 2), player.y - (player.height / 2), player.width, player.height);
+        ctx.fillStyle = '#fff';
+        // ctx.fillRect(200, 200, 100, 100);
+        ctx.fillRect(player.x - (player.width / 2), player.y - (player.height / 2), player.width, player.height);
+        ctx.fill();
 
     }
 
@@ -20,9 +28,31 @@ export default function Pong() {
         let frameCount = 0;
         let animationFrameId;
 
+
+        class Player{
+            constructor(x, y){
+                this.x = x;
+                this.y = y;
+
+                this.height = 150;
+                this.width = 50;
+            }
+        }
+
+        let player1 = new Player(canvas.width * 0.2, context.canvas.height * 0.5);
+        let player2 = new Player(canvas.width * 0.8, context.canvas.height * 0.5);
+
+        console.log(player1, player2);
+
         function render(){
+            context.clearRect(0, 0, canvas.width, canvas.height);
             frameCount++;
-            draw(context, frameCount);
+            drawBg(context, canvas);
+
+            // console.log(player1.x, player1.y);
+
+            drawPlayer(context, player1);
+            drawPlayer(context, player2);
             animationFrameId = window.requestAnimationFrame(render);
         }
 
@@ -37,7 +67,7 @@ export default function Pong() {
     return (
         <>
             <div>
-                <canvas ref={canvasRef} id="pongGame" width="500" height="500">
+                <canvas ref={canvasRef} id="pongGame" width="1800" height="800" style={{margin: "0"}}>
 
                 </canvas>
             </div>
